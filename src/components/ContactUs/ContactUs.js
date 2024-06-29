3534ff910adea168e0f9302f2738e978c88d8f85
Ken Binoy <91172526+ken-binoy@users.noreply.github.com>
2024-06-29 17:04:00 +0530

    Update ContactUs.js

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { SocialIcon } from "react-social-icons";
-import contactus from "../../Assets/81732-contact-us.gif"
+import contactus from "../../Assets/81732-contact-us.gif";
 
 function ContactUs() {
   return (
     <Container fluid className="project-section">
       <Container>
-        <h1 className="project-heading">
-          Contact Us
-        </h1>
+        <h1 className="project-heading">Contact Us</h1>
         <p style={{ color: "gray" }}>
           Whether you have questions, feedback, or inquiries, simply click on the social icons. Your thoughts matter to us,
-
-          and we are here to assist you every step of the way.Reach out today!
+          and we are here to assist you every step of the way. Reach out today!
         </p>
         <SocialIcon
           url="https://www.linkedin.com/in/athundev-sudevan-4b7306248/"
@@ -21,7 +18,7 @@ function ContactUs() {
           bgColor="transparent"
         />
         <SocialIcon
-          url="mailto: athunchindu1234@gmail.com"
+          url="mailto:athunchindu1234@gmail.com"
           fgColor="grey"
           bgColor="transparent"
         />
@@ -41,14 +38,31 @@ function ContactUs() {
           bgColor="transparent"
         />
         <Row style={{ justifyContent: "center" }}>
-        <Col md={5} style={{ paddingBottom: 10 }}>
-              <img
-                src={contactus}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
-            </Col>
+          <Col md={5} style={{ paddingBottom: 10 }}>
+            <img
+              src={contactus}
+              alt="home pic"
+              className="img-fluid"
+              style={{ maxHeight: "450px" }}
+            />
+          </Col>
+        </Row>
+        <Row
+          style={{
+            position: "absolute",
+            bottom: "10px",
+            left: "10px",
+            color: "gray"
+          }}
+        >
+          <Col md={12} style={{ textAlign: "left" }}>
+            <address>
+              Angelinterface IT Solutions<br />
+              B-156 Eric Crescent<br />
+              Timmins, ON<br />
+              P0N 1K0
+            </address>
+          </Col>
         </Row>
       </Container>
     </Container>
